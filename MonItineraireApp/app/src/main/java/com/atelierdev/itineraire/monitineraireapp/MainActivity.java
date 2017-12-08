8bd108c00cb03bbdba86c3afda74fbd6fd13ac1d
Emmanuel de Revel <emmanuel.de-revel@student.ecp.fr>
2017-12-08 17:14:15 +0100

    carte

--- MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/MainActivity.java
+++ MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/MainActivity.java
@@ -36,15 +36,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     /**
@@ -122,5 +113,10 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_POINTB, pointB);
         startActivity(intent);
 
+    }
+
+    public void displayMap(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
