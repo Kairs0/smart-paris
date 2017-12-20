67319cadf7ceeaccd9c23eb1577dfcac5642f670
Kairs0 <arnaud.convers@gmail.com>
2017-12-20 23:36:14 +0100

    ajout autocompletion

--- MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/MainActivity.java
+++ MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.atelierdev.itineraire.monitineraireapp;
 
 import android.app.AlertDialog;
+import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -11,11 +12,19 @@ import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -35,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
     private Double longitudeUser;
     private Double latitudeUser;
 
+    private LatLng startPoint;
+    private LatLng endPoint;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,96 +62,142 @@ public class MainActivity extends AppCompatActivity {
             locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER,
                     2000,
                     10, locationListenerGPS);
-        } catch (SecurityException e){ }
+        } catch (SecurityException e){ /*TODO*/}
         finally {
             isLocationEnabled();
         }
 
-    }
 
+        // AUTO COMPLETE POINT A AND B
+        PlaceAutocompleteFragment autocompleteStartPoint = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.pointA);
 
-    /**
-     * Called when the user press info button
-     */
-    public void displayInfo(View view){
-        Intent intent = new Intent(this, DisplayInfoMonument.class);
-        EditText editMonument = (EditText) findViewById(R.id.monument);
+        PlaceAutocompleteFragment autocompleteEndPoint = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.pointB);
 
-        String monument = editMonument.getText().toString();
+        /*
+        * The following code example shows setting an AutocompleteFilter on a PlaceAutocompleteFragment to
+        * set a filter returning only results with a precise address.
+        */
+        AutocompleteFilter addressFilter = new AutocompleteFilter.Builder()
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_ADDRESS)
+                .build();
 
-        intent.putExtra(EXTRA_MONUMENT, monument);
-        startActivity(intent);
+        autocompleteStartPoint.setFilter(addressFilter);
+        autocompleteEndPoint.setFilter(addressFilter);
+
+        autocompleteStartPoint.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                setStartPoint(place.getLatLng());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+            }
+        });
+
+        autocompleteEndPoint.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                setEndPoint(place.getLatLng());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO
+            }
+        });
+
     }
 
     public void displayMap(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
 
-        EditText editTextA = (EditText) findViewById(R.id.pointA);
-        EditText editTextB = (EditText) findViewById(R.id.pointB);
+        // TODO points intermediaires
         EditText editTextInt = (EditText) findViewById(R.id.pointInt);
+        String pointInt = editTextInt.getText().toString();
 
         String pointA;
-        String pointB = editTextB.getText().toString();
-        String pointInt = editTextInt.getText().toString();
+        String pointB = this.endPoint != null ?
+                String.valueOf(this.endPoint.latitude) + "," + String.valueOf(this.endPoint.longitude):
+                "";
 
         if(this.useMyLocForMap){
             pointA = String.valueOf(this.latitudeUser) + "," + String.valueOf(this.longitudeUser);
         } else {
-            pointA = editTextA.getText().toString();
+            pointA = this.startPoint != null ?
+                    String.valueOf(this.startPoint.latitude) + "," + String.valueOf(this.startPoint.longitude) :
+                    "";
         }
 
-        intent.putExtra(EXTRA_POINTA, pointA);
-        intent.putExtra(EXTRA_POINTB, pointB);
-        intent.putExtra(EXTRA_POINTSUPP, pointInt);
+        boolean searchAllowed = true;
 
-        if (TextUtils.isEmpty(pointA) || TextUtils.isEmpty(pointB)){
-            this.isSearchAllowed = false;
+        if(TextUtils.isEmpty(pointA) || TextUtils.isEmpty(pointB)){
+            searchAllowed = false;
         }
 
-        if(!this.isSearchAllowed){
+        if (this.useMyLocForMap){
+            if (this.latitudeUser == null || this.longitudeUser == null){
+                searchAllowed = false;
+            }
+        }
+
+        if(!searchAllowed){
             String msg="Impossible d'effectuer une recherche !";
             Toast.makeText(mContext,msg,Toast.LENGTH_LONG).show();
         } else {
+            this.startPoint = null;
+            this.endPoint = null;
+            intent.putExtra(EXTRA_POINTA, pointA);
+            intent.putExtra(EXTRA_POINTB, pointB);
+            intent.putExtra(EXTRA_POINTSUPP, pointInt);
             startActivity(intent);
         }
     }
 
     public void showFieldWayPoint(View view){
+        // TODO: masquer lors du reclick, mettre autocomplte
         EditText editTextInt = (EditText) findViewById(R.id.pointInt);
         TextView textViewInt = (TextView) findViewById(R.id.pointIntTextView);
         editTextInt.setVisibility(View.VISIBLE);
         textViewInt.setVisibility(View.VISIBLE);
     }
 
-    public void useDeviceLocation(View view){
-
+    /**
+     * Si la checkbox "Utiliser ma localisation" est cochée,
+     * affiche la geolocalisation de l'utilisateur, sinon reaffiche l'input
+     * @param view
+     */
+    public void onUseDeviceLocationClick(View view){
         CheckBox checkBoxPos = (CheckBox) findViewById(R.id.use_loc);
+        EditText altText = (EditText) findViewById(R.id.pointA_alt);
 
-        EditText editTextA = (EditText) findViewById(R.id.pointA);
-        TextView textViewA = (TextView) findViewById(R.id.pointATextView);
+        Fragment fr = getFragmentManager().findFragmentById(R.id.pointA);
 
+        // TODO : utiliser strings litteral
         if (checkBoxPos.isChecked()){
             this.useMyLocForMap = true;
-            if (this.longitudeUser == null || this.longitudeUser == null){
-                editTextA.setText("Localisation impossible");
-                editTextA.setEnabled(false);
-                this.isSearchAllowed = false;
+            fr.getView().setVisibility(View.GONE);
+            altText.setVisibility(View.VISIBLE);
+            if (this.longitudeUser == null || this.latitudeUser == null){
+                altText.setText("Localisation impossible");
             } else {
-                editTextA.setText(this.latitudeUser + "," + this.longitudeUser);
-                editTextA.setEnabled(false);
-                this.isSearchAllowed = true;
+                altText.setText(this.latitudeUser + "," + this.longitudeUser);
             }
         } else {
-            editTextA.setText("");
-            editTextA.setEnabled(true);
+            altText.setText("");
+
+            altText.setVisibility(View.GONE);
+            fr.getView().setVisibility(View.VISIBLE);
+
             this.useMyLocForMap = false;
-            this.isSearchAllowed = true;
         }
     }
 
 
     //LOCATION
-
     LocationListener locationListenerGPS = new LocationListener() {
         @Override
         public void onLocationChanged(android.location.Location location) {
@@ -167,7 +225,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void isLocationEnabled() {
-
         if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
             AlertDialog.Builder alertDialog=new AlertDialog.Builder(mContext);
             alertDialog.setTitle("Activer la localisation");
@@ -196,19 +253,26 @@ public class MainActivity extends AppCompatActivity {
         this.latitudeUser = latitudeUser;
     }
 
+    public void setStartPoint(LatLng startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public void setEndPoint(LatLng endPoint) {
+        this.endPoint = endPoint;
+    }
+
     /**
-     * N'est plus utilisé (affichage du trajet sous forme de texte)
+     * Called when the user press info button
      */
-    public void calculTrajet(View view){
-        Intent intent = new Intent(this, DisplayPathAtoB.class);
-        EditText editTextA = (EditText) findViewById(R.id.pointA);
-        EditText editTextB = (EditText) findViewById(R.id.pointB);
+    public void displayInfo(View view){
+        Intent intent = new Intent(this, DisplayInfoMonument.class);
+        EditText editMonument = (EditText) findViewById(R.id.monument);
 
-        String pointA = editTextA.getText().toString();
-        String pointB = editTextB.getText().toString();
+        String monument = editMonument.getText().toString();
 
-        intent.putExtra(EXTRA_POINTA, pointA);
-        intent.putExtra(EXTRA_POINTB, pointB);
+        intent.putExtra(EXTRA_MONUMENT, monument);
         startActivity(intent);
     }
+
+
 }
