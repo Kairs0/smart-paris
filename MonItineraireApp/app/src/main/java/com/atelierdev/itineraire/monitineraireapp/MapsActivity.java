3ebe2a21308c98b7e7883e8e138b35d0ccf89069
Emmanuel de Revel <emmanuel.de-revel@student.ecp.fr>
2017-12-22 12:44:50 +0100

    rectangle + appartenance au rectangle

--- MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/MapsActivity.java
+++ MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/MapsActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -14,8 +15,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.maps.android.PolyUtil;
 
 
 import java.util.ArrayList;
@@ -117,13 +121,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             e.printStackTrace();
             // Créer des objets LatLng au niveau de l'hotel de ville, d'une origine et d'une destination
             LatLng paris = new LatLng(48.8566, 2.35177);
-            LatLng origine = new LatLng(48.856062, 2.353267);
+            LatLng origine = new LatLng(48.86, 2.36);
             LatLng destination = new LatLng(48.857460, 2.351670);
+            LatLng bhv = new LatLng(48.857734, 2.353692);
+            LatLng gervais = new LatLng(48.855731, 2.354099);
+            double ux=destination.latitude-origine.latitude;
+            double uy=destination.longitude-origine.longitude;
+            double ux_unitaire=ux/(Math.sqrt(Math.pow(ux,2)+Math.pow(uy,2)));
+            double uy_unitaire=uy/(Math.sqrt(Math.pow(ux,2)+Math.pow(uy,2)));
+            double ux_unitaire_normal=-uy_unitaire;
+            double uy_unitaire_normal=ux_unitaire;
+            double f=0.001;
+            LatLng S1 = new LatLng(origine.latitude+f*ux_unitaire_normal, origine.longitude+f*uy_unitaire_normal);
+            LatLng S2 = new LatLng(origine.latitude-f*ux_unitaire_normal, origine.longitude-f*uy_unitaire_normal);
+            LatLng S3 = new LatLng(destination.latitude+f*ux_unitaire_normal, destination.longitude+f*uy_unitaire_normal);
+            LatLng S4 = new LatLng(destination.latitude-f*ux_unitaire_normal, destination.longitude-f*uy_unitaire_normal);
             // Les mettre dans une liste
             List<LatLng> points=new ArrayList<LatLng>();
+            List<LatLng> poly=new ArrayList<LatLng>();
             points.add(origine);
             points.add(destination);
-            points.add(paris);
+            points.add(bhv);
+            points.add(gervais);
+            poly.add(S1);
+            poly.add(S2);
+            poly.add(S4);
+            poly.add(S3);
+            //points.add(paris);
             // Placer la camera au niveau du LatLng paris
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(paris, 18));
             //Ajouter les marqueurs de la liste
@@ -132,7 +156,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
             //Dessiner le trajet entre tous les points de la liste
             mMap.addPolyline(new PolylineOptions().addAll(points).width(5).color(Color.RED));
+            PolygonOptions rectOptions = new PolygonOptions().addAll(poly).fillColor(Color.BLACK);
 
+            // Get back the mutable Polygon
+            Polygon polygon = mMap.addPolygon(rectOptions);
+            Boolean boo = PolyUtil.containsLocation(gervais, poly, true);
+            Log.d("myTag", "Le point est-il dans le polygone ?"+ boo);
         }
     }
 
