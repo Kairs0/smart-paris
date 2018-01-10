1946df46e2c0eecfb5a3f16315ab28601c483e40
Kairs0 <arnaud.convers@gmail.com>
2018-01-10 20:11:18 +0100

    doc com for Matrix api use

--- MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/MatrixApiThread.java
+++ MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/MatrixApiThread.java
@@ -1,7 +1,5 @@
 package com.atelierdev.itineraire.monitineraireapp;
 
-import android.text.TextUtils;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -11,6 +9,44 @@ import java.util.List;
 
 /**
  * Created by Arnaud on 10/01/2018.
+ * Classe utilisée pour l'appel à l'API Google Matrix.
+ * Utilisation :
+ * 1) Instanciation de la classe
+ * >> MatrixApiThread example = new MatrixApiThread(List<String> origins, List<String> destinations, String mode);
+ * Avec origins liste d'origines sous forme de liste de Strings (coordonnées au format Lat,Lng ou adresse)
+ * Destinations liste de destinations sous forme de liste de Strings
+ * mode type de transport (walking pour à pied)
+ *
+ * 2) Appel asynchrone de la méthode run de la classe pour calculer le trajet
+ * >> Thread callThreadExample = new Thread(example);
+ * >> callThreadExample.start();
+ *
+ * 3) Récupération des résultats
+ * >> callThread.join(); // Attend la fin du thread
+ * >> String result = example.getResult(); stocke les résultats dans un string
+ *
+ * 4) Traitement du résultat avec la classe MatrixApiResultManager
+ * >> MatrixApiResultManager manageJsonExample = new MatrixApiResultManager(result);
+ * >> manageJsonExample.calculTime();
+ *
+ * 5) Appel des résultats
+ * >> List<List<Integer>> results = manageJsonExample.getTimesResult();
+ * Avec results list de list de integer,
+ * result.get(i).get(j) = tps en secondes de l'origine i à la destination j, où i et j commencent à 0
+ * par exemple, avec les paramètres suivants :
+ * Origines :
+ * Or_0 "Parvis Notre-Dame - Pl. Jean-Paul II, 75004 Paris-4E-Arrondissement, France",
+ * Or_1 "Place Charles de Gaulle, 75008 Paris, France",
+ * Or_2 "Avenue Anatole France, 75007 Paris, France"
+ * Destinations :
+ * Dest_0 "Boulevard du Palais, 75001 Paris, France",
+ * Dest_1 "Rue de Rivoli, 75004 Paris, France",
+ * Dest_2 "Rue du Faubourg Saint-Honoré, 75008 Paris, France"
+ * On a le tableau de temps suivant
+ * **********Or_0 to Dest_0**********Or_0 to Dest_1**********Or_0 to Dest_2
+ * **********Or_1 to Dest_0**********Or_1 to Dest_1**********Or_1 to Dest_2
+ * **********Or_2 to Dest_0**********Or_2 to Dest_1**********Or_2 to Dest_2
+ *
  */
 
 public class MatrixApiThread implements Runnable {
