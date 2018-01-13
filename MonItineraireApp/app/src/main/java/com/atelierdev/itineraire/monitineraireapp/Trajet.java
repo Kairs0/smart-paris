b673e396e16fe7a33dd21a2d52bd5892042f9cd5
Elodie <elodie.ikkache@gmail.com>
2018-01-13 12:59:45 +0100

    test pour l'algo (et corrections)

--- MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/Trajet.java
+++ MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/Trajet.java
@@ -42,46 +42,56 @@ public class Trajet {
         this.temps_sous_parcours.add(tps_parcours);
         this.matrice_temps = matrice_temps;
         this.ordre_matrice = ordre_matrice;
+        construction_trajet();
 
     }
 
     //Methode pour ajouter un monument au trajet
     public void construction_trajet() {
         //TODO parametre de temps minimal pour continuer à chercher?
-        while ((this.temps_parcours < this.duree_souhaitee) || (this.monuments_interet.isEmpty() == false)){
+        while ((this.temps_parcours < this.duree_souhaitee) && (this.monuments_interet.isEmpty() == false)){
             //on prend le monument avec le plus grand interet de la zone
             Monument monument = this.monuments_interet.remove(0);
 
             //on calcule ou integrer le nouveau monument
             int l = this.trajet.size();
             int bat_pos = 0;
-            int tps1 = get_temps(A.toString(), monument.getName());
-            int tps2 = get_temps(monument.getName(), this.trajet.get(0).getName());
-            int temps_min = tps1 + tps2- this.temps_sous_parcours.get(0);
-            for(int m = 0; m < l - 1; m++)
-            {
-                int t1= get_temps(this.trajet.get(m).getName(), monument.getName());
-                int t2 = get_temps(monument.getName(), this.trajet.get(m+1).getName());
-                int t =  t1 + t2 - this.temps_sous_parcours.get(m +1);
-                if (t<temps_min) {
-                    bat_pos = m + 1;
+            int tps1 = 0;
+            int tps2 = 0;
+            int temps_min = 0;
+            if (l == 0) {
+                tps1 = get_temps("A", monument.getName());
+                tps2 = get_temps(monument.getName(), "B");
+                temps_min = tps1 + tps2 - this.temps_sous_parcours.get(0);
+            }
+            else{
+                tps1 = get_temps("A", monument.getName());
+                tps2 = get_temps(monument.getName(), this.trajet.get(0).getName());
+                temps_min = tps1 + tps2 - this.temps_sous_parcours.get(0);
+                for (int m = 0; m < l - 1; m++) {
+                    int t1 = get_temps(this.trajet.get(m).getName(), monument.getName());
+                    int t2 = get_temps(monument.getName(), this.trajet.get(m + 1).getName());
+                    int t = t1 + t2 - this.temps_sous_parcours.get(m + 1);
+                    if (t < temps_min) {
+                        bat_pos = m + 1;
+                        temps_min = t;
+                        tps1 = t1;
+                        tps2 = t2;
+                    }
+                }
+                int t1 = get_temps(this.trajet.get(l - 1).getName(), monument.getName());
+                int t2 = get_temps(monument.getName(), "B");
+                int t = t1 + t2 - this.temps_sous_parcours.get(l);
+                if (t < temps_min) {
+                    bat_pos = l;
                     temps_min = t;
                     tps1 = t1;
                     tps2 = t2;
                 }
             }
-            int t1 = get_temps(this.trajet.get(l - 1).getName(), monument.getName());
-            int t2 = get_temps(monument.getName(), B.toString());
-            int t =  t1 + t2 - this.temps_sous_parcours.get(l);
-            if (t<temps_min) {
-                bat_pos = l;
-                temps_min = t;
-                tps1 = t1;
-                tps2 = t2;
-            }
-
             //on insere le monument au trajet si le temps le permet
             if (this.temps_parcours + temps_min < this.duree_souhaitee) {
+                this.trajet.add(bat_pos, monument);
                 int t_visite = monument.getVisitTime();
                 if (this.temps_parcours + temps_min + t_visite < this.duree_souhaitee){
                     this.temps_de_visite.add(bat_pos, t_visite);
@@ -93,8 +103,8 @@ public class Trajet {
                     this.temps_parcours += temps_min;
                 }
                 this.temps_sous_parcours.remove(bat_pos);
-                this.temps_sous_parcours.add(bat_pos, tps1);
                 this.temps_sous_parcours.add(bat_pos, tps2);
+                this.temps_sous_parcours.add(bat_pos, tps1);
 
             }
         }
@@ -105,7 +115,7 @@ public class Trajet {
         int l = this.ordre_matrice.size();
         int a = 0;
         int b = 0;
-        for(int i = 0; i < l - 1; i++)
+        for(int i = 0; i < l; i++)
         {
             if (this.ordre_matrice.get(i) == m1) {
                 a = i;
