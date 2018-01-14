cb378cf746d73ffaef7320d8d1d0b5dc490cd0d5
Kairs0 <arnaud.convers@gmail.com>
2018-01-14 18:27:02 +0100

    add exception sur sublist + marqueurs verts/jaune + optimisation google direction

--- MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/MapsActivity.java
+++ MonItineraireApp/app/src/main/java/com/atelierdev/itineraire/monitineraireapp/MapsActivity.java
@@ -113,7 +113,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // TODO: AUGMENTER VALEUR A PLUS DE BATIMENT SANS POUR AUTANT PETER L'API
         // On s'intéresse aux 4 premiers monuments pour la contrainte de l'api matrix
-        List<Monument> restrainedMonumentList = relevantMonuments.subList(0, 4);
+        List<Monument> restrainedMonumentList;
+        try {
+            restrainedMonumentList = relevantMonuments.subList(0, 4);
+        } catch (IndexOutOfBoundsException e){
+            if (relevantMonuments.size() > 0){
+                int i_max = relevantMonuments.size() - 1;
+                restrainedMonumentList = relevantMonuments.subList(0, i_max);
+            } else {
+                restrainedMonumentList = new ArrayList<>();
+            }
+        }
+
+        List<Monument> monumentsOnPath = new ArrayList<>(restrainedMonumentList);
 
         // Construction de la liste des coordonnées de l'ensemble des points intéressant + pointA et B
         List<String> listCoords = new ArrayList<>();
@@ -125,6 +137,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Appelle l'api matrix et retourne la matrice des distances
         List<List<Integer>> matrix = getMatrix(listCoords, listCoords, "walking");
+
+        if(matrix.size() == 0){
+            setErrorMessage("La route n'a pas été trouvée suite à un problème interne à l'application");
+            return;
+        }
 
         // Temps souhaité par l'utilisateur
         int temps_souhaite_sec = Integer.parseInt(temps_disponible_h.replaceAll("h", "")) * 60 * 60 +
@@ -166,28 +183,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         InitalizeMapForPath(pointsPath);
 
         //Dictionnaire pour associer les ids (valeur) des monuments à leur titre (clé) et les récupérer lors d'un clic sur l'étiquette
-        final HashMap<String, String> markerIds = new HashMap<>();
-
-        //Ajoute un marqueur vert aux monuments qui sont dans la zone et un marqueur jaune sinon
-        for (int i = 0; i < allRelevantMonument.size(); i++) {
-            Monument monument = allRelevantMonument.get(i);
-            LatLng latlng = new LatLng(monument.getLat(), monument.getLon());
-            String monument_id_str = Integer.toString(monument.getMonumentId());
-            if (PolyUtil.containsLocation(latlng, rectangleInteret, true)) {
-                mMap.addMarker(new MarkerOptions().position(latlng).title(monument.getName()).icon(BitmapDescriptorFactory
-                        .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-                Marker marker = mMap.addMarker(new MarkerOptions()
-                        .position(latlng)
-                        .title(monument.getName())
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-                markerIds.put(marker.getTitle(), monument_id_str);
-//                selected_monuments.add(monument);
-            } else {
-                Marker marker = mMap.addMarker(new MarkerOptions().position(latlng).title(monument.getName()).icon(BitmapDescriptorFactory
-                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
-                markerIds.put(marker.getTitle(), monument_id_str);
-            }
-        }
+        final HashMap<String, String> markerIds = putMarkersOnMonuments(allRelevantMonument, monumentsOnPath);
 
         googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
 
@@ -219,6 +215,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         optionsMap.width(5);
         optionsMap.color(Color.BLUE);
         mMap.addPolyline(optionsMap);
+    }
+
+    private HashMap<String, String> putMarkersOnMonuments(List<Monument> allRelevantMonument, List<Monument> monumentsPath){
+        HashMap<String, String> markerIds = new HashMap<>();
+
+        //Ajoute un marqueur vert aux monuments qui sont dans le trajet et un marqueur jaune sinon
+        for (int i = 0; i < allRelevantMonument.size(); i++) {
+            Monument monument = allRelevantMonument.get(i);
+            LatLng latlng = new LatLng(monument.getLat(), monument.getLon());
+            String monument_id_str = Integer.toString(monument.getMonumentId());
+            if (monumentsPath.contains(monument)) {
+                mMap.addMarker(new MarkerOptions().position(latlng).title(monument.getName()).icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                Marker marker = mMap.addMarker(new MarkerOptions()
+                        .position(latlng)
+                        .title(monument.getName())
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                markerIds.put(marker.getTitle(), monument_id_str);
+//                selected_monuments.add(monument);
+            } else {
+                Marker marker = mMap.addMarker(new MarkerOptions().position(latlng).title(monument.getName()).icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+                markerIds.put(marker.getTitle(), monument_id_str);
+            }
+        }
+        return  markerIds;
     }
 
     private String callApiDirectionAndGetJson(String pointA, String pointB, String mode,  List<String> waypoints){
@@ -320,7 +342,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             // We set here the time maximum for waiting the result from api
             Future<?> f = service.submit(callThread);
-            f.get(5, TimeUnit.MINUTES);
+            f.get(15, TimeUnit.SECONDS);
 
             callThread.join();
             result = api.getResult();
